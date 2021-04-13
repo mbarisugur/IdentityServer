471bc88d32f9fabed6ea92d44b2b50da2fc25ec5
Mustafa Barış Uğur <73464837+mbarisugur@users.noreply.github.com>
2021-04-13 11:41:17 +0300

    Initial commit

---
+++ IdentityServer.AuthServer/Config.cs
@@ -0,0 +1,135 @@
+﻿using IdentityServer4;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace IdentityServer.AuthServer
+{
+    public static class Config
+    {
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>()
+            {
+                new ApiResource("resource_api1"){
+                    Scopes ={ "api1.read", "api1.write", "api1.update" },
+                    ApiSecrets = new[]{new Secret("secretapi1".Sha256()) }
+                },
+                new ApiResource("resource_api2"){
+                    Scopes = { "api2.read", "api2.write", "api2.update" },
+                    ApiSecrets = new[]{new Secret("secretapi1".Sha256()) }
+                }
+            };
+        }
+
+        public static IEnumerable<ApiScope> GetApiScopes()
+        {
+            return new List<ApiScope>()
+            {
+                new ApiScope("api1.read","API 1 için okuma iznidir."),
+                new ApiScope("api1.write","API 1 için yazma iznidir."),
+                new ApiScope("api1.update","API 1 için güncelleme iznidir."),
+                new ApiScope("api2.read","API 2 için okuma iznidir."),
+                new ApiScope("api2.write","API 2 için yazma iznidir."),
+                new ApiScope("api2.update","API 2 için güncelleme iznidir."),
+                new ApiScope("api3.read","API 3 için okuma iznidir."),
+                new ApiScope("api3.write","API 3 için yazma iznidir."),
+                new ApiScope("api3.update","API 3 için güncelleme iznidir.")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>()
+            {
+                new Client()
+                {
+                    ClientId = "Client1",
+                    ClientName = "Client 1 App",
+                    ClientSecrets = new[]{new Secret("secret".Sha256())},
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    AllowedScopes = {"api1.read"}
+                },
+                new Client()
+                {
+                    ClientId = "Client2",
+                    ClientName = "Client 2 App",
+                    ClientSecrets = new[]{new Secret("secret".Sha256())},
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    AllowedScopes = {"api1.read","api1.update","api2.write","api2.update"}
+                },
+                 new Client()
+                {
+                    ClientId = "Client1-Mvc",
+                    RequirePkce = false,
+                    ClientName = "Client 1 App",
+                    ClientSecrets = new[]{new Secret("secret".Sha256())},
+                    AllowedGrantTypes = GrantTypes.Hybrid,
+                    RedirectUris = new List<string>{"https://localhost:5003/signin-oidc" },
+                    PostLogoutRedirectUris = new List<string>{ "https://localhost:5003/signout-callback-oidc" },
+                    AllowedScopes = {IdentityServerConstants.StandardScopes.OpenId, IdentityServerConstants.StandardScopes.Profile, "api1.read",IdentityServerConstants.StandardScopes.OfflineAccess, "Roles"},
+                    AccessTokenLifetime  = 2*60*60,
+                    AllowOfflineAccess = true,
+                    RefreshTokenUsage = TokenUsage.ReUse,
+                    AbsoluteRefreshTokenLifetime = (DateTime.Now.AddDays(60)-DateTime.Now).Seconds,
+                    RequireConsent = true
+                },
+                 new Client()
+                {
+                    ClientId = "Client2-Mvc",
+                    RequirePkce = false,
+                    ClientName = "Client 2 App",
+                    ClientSecrets = new[]{new Secret("secret".Sha256())},
+                    AllowedGrantTypes = GrantTypes.Hybrid,
+                    RedirectUris = new List<string>{"https://localhost:5005/signin-oidc" },
+                    PostLogoutRedirectUris = new List<string>{ "https://localhost:5005/signout-callback-oidc" },
+                    AllowedScopes = {IdentityServerConstants.StandardScopes.OpenId, IdentityServerConstants.StandardScopes.Profile, "api1.read",IdentityServerConstants.StandardScopes.OfflineAccess, "Roles"},
+                    AccessTokenLifetime  = 2*60*60,
+                    AllowOfflineAccess = true,
+                    RefreshTokenUsage = TokenUsage.ReUse,
+                    AbsoluteRefreshTokenLifetime = (DateTime.Now.AddDays(60)-DateTime.Now).Seconds,
+                    RequireConsent = true
+                }
+            };
+        }
+
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>()
+            {
+                new IdentityResources.OpenId(), //subId
+                new IdentityResources.Profile(),
+                new IdentityResource(){Name = "Roles", DisplayName = "Roles", Description = "Kullanıcı Rolleri", UserClaims = new[]{"role"}}
+             };
+        }
+        public static IEnumerable<TestUser> GetUsers()
+        {
+            return new List<TestUser>()
+            {
+                new TestUser
+                {
+                    SubjectId = "1", Username ="mbarisugur",Password= "password", Claims = new List<Claim>()
+                    {
+                        new Claim("given_name","Barış"),
+                        new Claim("family_name", "Uğur"),
+                        new Claim("role","user")
+                    }
+               },
+                new TestUser
+                {
+                    SubjectId = "2", Username ="onurdogan",Password= "password", Claims = new List<Claim>()
+                    {
+                        new Claim("given_name","Onur"),
+                        new Claim("family_name", "Dogan"),
+                        new Claim("role","admin")
+                    }
+               }
+            };
+        }
+
+    }
+}
---
+++ IdentityServer.Client1/Controllers/UsersController.cs
@@ -0,0 +1,125 @@
+﻿using IdentityModel.Client;
+using IdentityServer.Client1.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace IdentityServer.Client1.Controllers
+{
+    [Authorize]
+    public class UsersController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public UsersController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public async Task<IActionResult> Index()
+        {
+            HttpClient httpClient = new HttpClient();
+            List<User> users = new List<User>();
+            //var discovery = await httpClient.GetDiscoveryDocumentAsync("https://localhost:5001");
+            //if (discovery.IsError)
+            //{
+            //    Console.WriteLine("Discovery hatası alındı.");
+            //}
+            //ClientCredentialsTokenRequest clientCredentialsToken = new ClientCredentialsTokenRequest();
+
+            //clientCredentialsToken.ClientId = _configuration["Client:ClientId"];
+            //clientCredentialsToken.ClientSecret = _configuration["Client:ClientSecret"];
+            //clientCredentialsToken.Address = discovery.TokenEndpoint;
+
+            //var token = await httpClient.RequestClientCredentialsTokenAsync(clientCredentialsToken);
+
+            var accessToken = await HttpContext.GetTokenAsync(OpenIdConnectParameterNames.AccessToken);
+
+            httpClient.SetBearerToken(accessToken);
+
+            var response = await httpClient.GetAsync("https://localhost:5007/api/users/getusers");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                users = JsonConvert.DeserializeObject<List<User>>(content);
+            }
+            else
+            {
+                Console.WriteLine("response hatası");
+            }
+
+            return View(users);
+        }
+
+        public IActionResult UserInfo()
+        {
+            return View();
+        }
+
+
+        public async Task LogOut()
+        {
+            await HttpContext.SignOutAsync("Cookies");
+            await HttpContext.SignOutAsync("oidc");
+        }
+
+        public async Task<IActionResult> GetRefreshToken()
+        {
+            var refreshToken = await HttpContext.GetTokenAsync(OpenIdConnectParameterNames.RefreshToken);
+            HttpClient httpClient = new HttpClient();
+            var discovery = await httpClient.GetDiscoveryDocumentAsync("https://localhost:5001");
+
+            RefreshTokenRequest refreshTokenRequest = new RefreshTokenRequest();
+            refreshTokenRequest.ClientId = _configuration["Client1Mvc:ClientId"];
+            refreshTokenRequest.ClientSecret = _configuration["Client1Mvc:ClientSecret"];
+            refreshTokenRequest.RefreshToken = refreshToken;
+            refreshTokenRequest.Address = discovery.TokenEndpoint;
+            var token = await httpClient.RequestRefreshTokenAsync(refreshTokenRequest);
+            if (token.IsError)
+            {
+                Console.WriteLine("Token hatası !");
+            }
+            var tokens = new List<AuthenticationToken>()
+            {
+                new AuthenticationToken{Name = OpenIdConnectParameterNames.IdToken, Value = token.IdentityToken},
+                new AuthenticationToken{Name = OpenIdConnectParameterNames.AccessToken, Value = token.AccessToken},
+                new AuthenticationToken{Name = OpenIdConnectParameterNames.RefreshToken, Value = token.RefreshToken},
+                new AuthenticationToken{Name = OpenIdConnectParameterNames.ExpiresIn, Value = DateTime.UtcNow.AddSeconds(token.ExpiresIn).ToString("o",CultureInfo.InvariantCulture)}
+            };
+
+            var authenticationResult = await HttpContext.AuthenticateAsync();
+            var properties = authenticationResult.Properties;
+
+
+            properties.StoreTokens(tokens);
+
+            await HttpContext.SignInAsync("Cookies", authenticationResult.Principal, properties);
+
+            return RedirectToAction("Index");
+        }
+
+        [Authorize(Roles ="admin")]
+        public IActionResult AdminAction()
+        {
+            // sdfasflkdfjasdjfklajsdkfljaskf
+
+            return View();
+        }
+
+
+        [Authorize(Roles = "user")]
+        public IActionResult UserAction()
+        {
+            return View();
+        }
+
+    }
+}
